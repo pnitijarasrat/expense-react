5a18212b76378ab74a40521943f64af5f010dcdb
pnitijarasrat <puriwatds@gmail.com>
2023-01-04 16:23:42 +0700

    fix modal and overflow x bug

--- src/component/Modal/Modal.js
+++ src/component/Modal/Modal.js
@@ -1,12 +1,11 @@
 import classes from './Modal.module.css'
-import Backdrop from './Backdrop'
 import Button from '../UI/Button/Button'
 
 const Modal = (props) => {
   return (
     <div className={classes.Modal}>
       <h1>{props.header}</h1>
-      <p style={{ width: '100%' }}>{props.modalMessage}</p>
+      <p style={{ width: '100%', overflow: 'auto' }}>{props.modalMessage}</p>
       <Button onClick={props.onClickButton}>Okay</Button>
     </div>
   )
